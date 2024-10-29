0e01d7247f353b20a636caf80157e2a864a3c472
Abraham015 <ahernadeza1607@alumno.ipn.mx>
2024-10-29 17:33:36 -0600

    Circuit breaker implemented

--- services/order-service/src/main/java/dev/abraham/order/client/InventoryClient.java
+++ services/order-service/src/main/java/dev/abraham/order/client/InventoryClient.java
@@ -1,8 +1,9 @@
 package dev.abraham.order.client;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @FeignClient(
@@ -11,5 +12,11 @@ import org.springframework.web.bind.annotation.RequestParam;
 )
 public interface InventoryClient {
     @GetMapping
+    @CircuitBreaker(name = "inventory", fallbackMethod = "fallbackMethod")
+    @Retry(name = "inventory")
     boolean isInStock(@RequestParam String skuCode, @RequestParam Integer quantity);
+
+    default boolean fallbackMethod(String code, Integer quantity, Throwable throwable) {
+        return false;
+    }
 }
